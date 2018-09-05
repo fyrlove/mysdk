4d472c560bf18a20e2a0fe5b2af527d85e41eb58
Jeff.Feng <554389997@qq.com>
2018-09-05 16:20:59 +0800

    新建BaseActivity，与业务无关的基类，提供3个抽象方法，统一Activity中的代码风格

---
+++ vuandroidadsdk/src/main/java/com/ydky/vuandroidadsdk/activity/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.ydky.vuandroidadsdk.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initVariables();
+        initViews(savedInstanceState);
+        loadData();
+    }
+
+    protected abstract void initVariables();
+    protected abstract void initViews(Bundle saveInstanceState);
+    protected abstract void loadData();
+}
